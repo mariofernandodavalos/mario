42ea0ba06a747aaeec951efa0c68d40137fcf4b8
Mario Fernando Davalos Navarro <mariofernandodavalos@hotmail.com>
2016-10-15 20:46:17 -0500

    Version 1.0.0.1

--- VieTaxi/ViewController.swift
+++ VieTaxi/ViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 import QuartzCore
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
     
     @IBOutlet weak var UserLabel: UITextField!
@@ -20,12 +20,23 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var DataLogin: UIStackView!
     
+    @IBOutlet weak var Logo: UIImageView!
+    
+    var LogoSave: UIImageView!
+    var LogoChan: UIImageView!
+    
+     static let MAX_TEXT_SIZE = 30
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.Ingresar.layer.cornerRadius = 15.0
         self.UserLabel.layer.cornerRadius = 15.0
         self.PassLabel.layer.cornerRadius = 15.0
+        LogoSave = Logo
+        
+        
+        
     }
     
     override func didReceiveMemoryWarning() {
@@ -33,6 +44,21 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    //MARK: Metodos del text Field delegate
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        guard let text = textField.text else { return true }
+        let newLength = text.characters.count + string.characters.count - range.length
+        return newLength <= ViewController.MAX_TEXT_SIZE
+    }
+    
+    @IBAction func Focus(_ sender: UITextField) {
+        Logo.isHidden = false
+        
+    }
+    
+    @IBAction func NoFocus(_ sender: UITextField) {
+        Logo.isHidden = true
+    }
+    
 }
 
